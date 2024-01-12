9d251e6149af0dcb47680100a1b83855d614169b
eorev <eorev@udel.edu>
2024-01-12 01:52:46 -0500

    fixed index file showing

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Sorting Visualizer</h1>
+            <p>Welcome to the Sorting Visualizer!</p>
+            {/* Add your sorting visualizer component here */}
+        </div>
+    );
+};
+
+export default HomePage;
