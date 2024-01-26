137050b2ef23f65b7e98dd2a41171f9a38ada5be
eorev <eorev@udel.edu>
2024-01-26 18:39:21 -0500

    Updated Readme and index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,7 @@ import SortingVisualizer from "@/app/components/SortingVisualizer";
 const HomePage: React.FC = () => {
     return (
         <div>
-            <h1>Sorting Visualizer</h1>
-            <p>Welcome to the Sorting Visualizer!</p>
-            <SortingVisualizer />  
+            <SortingVisualizer />
         </div>
     );
 };
